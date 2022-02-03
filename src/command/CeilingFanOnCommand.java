ec41ab8373830f96ccb956438ddae1e58cd39e52
J.H. Hsu <juihsiuhsu@gmail.com>
2022-02-03 14:03:34 +0800

    Add command pattern

---
+++ src/command/CeilingFanOnCommand.java
@@ -0,0 +1,19 @@
+package command;
+
+public class CeilingFanOnCommand implements Command {
+    CeilingFan ceilingFan;
+
+    public CeilingFanOnCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        ceilingFan.high();
+    }
+
+    @Override
+    public void undo() {
+        ceilingFan.off();
+    }
+}
