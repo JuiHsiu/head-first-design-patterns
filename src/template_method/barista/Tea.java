7dfaa30d4586b9dfa5d0774ce724f1aad4701406
J.H. Hsu <juihsiuhsu@gmail.com>
2022-05-22 16:30:08 +0800

    Add template method pattern

---
+++ src/template_method/barista/Tea.java
@@ -0,0 +1,13 @@
+package template_method.barista;
+
+public class Tea extends CaffeineBeverage {
+    @Override
+    public void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+    @Override
+    public void addCondiments() {
+        System.out.println("Adding Lemon");
+    }
+}
